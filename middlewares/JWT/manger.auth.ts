d38cd6aae5dd9de0b1b4beab2e01baee25929616
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-08 16:47:25 -0800

    saved changes

---
+++ middlewares/JWT/manger.auth.ts
@@ -0,0 +1,7 @@
+import { Request , Response , NextFunction } from "express";
+import { IAuthUser } from "../../interfaces/User";
+
+
+export const isManager = (req:IAuthUser , res: Response , next : ) =>{
+
+}
