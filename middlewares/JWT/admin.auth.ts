af4b0435f2028350d8b98ede611f24a65160fa43
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-13 15:51:35 -0800

    updated

--- middlewares/JWT/admin.auth.ts
+++ middlewares/JWT/admin.auth.ts
@@ -1,13 +1,15 @@
-import { Request , Response , NextFunction} from "express";
-import { IAuthUser } from "../../interfaces/User";
+import { Request, Response, NextFunction } from "express";
+import { IAuthUser, Iuser } from "../../interfaces/User";
 
+export const isAdmin = (req: IAuthUser, res: Response, next: NextFunction) => {
+  const user = req.user as Iuser;
+  if (user && user.role === "admin") {
+    next();
+  } else {
+    res
+      .status(401)
+      .json({ message: "You are not authorized to perform this action" });
+  }
+};
 
-export const isAdmin = (req: IAuthUser , res:Response , next:NextFunction) =>{
-    const user = req.user;
-    if(user && user.role === 'admin'){
-        next();
-    }else{
-        res.status(401).json({message: 'You are not authorized to perform this action'});
-    }
-}
-
+export const IsAdmin = (req: Request, res: Response, next: NextFunction) => {};
