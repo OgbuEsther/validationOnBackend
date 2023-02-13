aaffbc136287adc96ffad7180d18bf541c212c4d
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-13 11:02:22 -0800

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
-import { env } from "process";
 import { envVariable } from "../config/enviornmentVariables";
 import { appConfig } from "./app";
 import { dbConfig } from "../config/db";
@@ -12,9 +11,9 @@ const app: Application = express();
 appConfig(app);
 dbConfig();
 
-// app.get("/", (req: Request, res: Response) => {
-//   return res.status(200).send("server is up and running");
-// });
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).send("server is up and running");
+});
 
 app.listen(Port, () => {
   console.log(`server is up on port ${Port}`);
