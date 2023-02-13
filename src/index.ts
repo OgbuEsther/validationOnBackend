15c9b9617e725346eb2d2570db2fa1282e24d778
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-13 11:00:38 -0800

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import express, { Application } from "express"
-import dotenv from "dotenv"
-import { env } from "process"
-import { envVariable } from "../config/enviornmentVariables"
-import { appConfig } from "./app"
-import { dbConfig } from "../config/db"
-dotenv.config()
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import { env } from "process";
+import { envVariable } from "../config/enviornmentVariables";
+import { appConfig } from "./app";
+import { dbConfig } from "../config/db";
+dotenv.config();
 
+const Port = envVariable.PORT;
 
-const Port = envVariable.PORT
+const app: Application = express();
+appConfig(app);
+dbConfig();
 
-const app : Application =express()
-appConfig(app)
-dbConfig()
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).send("server is up and running");
+});
 
-app.listen(Port , () =>{
-    console.log(`server is up on port ${Port}`)
-})
+app.listen(Port, () => {
+  console.log(`server is up on port ${Port}`);
+});
