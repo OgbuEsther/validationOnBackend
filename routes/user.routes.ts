5a04b85cccce4e60ef23ae82213a4a99ca83c519
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-16 00:46:15 -0800

    updated

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,3 +1,17 @@
 import { Router } from "express";
+import { LoginUsers, register, getAll } from "../controller/user.controls";
+import { userAuth } from "../middlewares/JWT/user.auth";
 
-const router = Router()
+import {
+  registerValidation,
+  loginValidation,
+} from "../middlewares/validation/authValidation/userValidation";
+const router = Router();
+
+router.route("/register").post(registerValidation, register);
+
+router.route("/login").post(loginValidation, LoginUsers);
+
+router.route("/").get(userAuth, getAll);
+
+export default router;
